723254ea673a2d823acb8911c210e373d731fb5d
Lavelle Brown <lavellebrown@yahoo.com>
2017-01-23 09:28:59 -0500

    First commit

---
+++ VirtualPet/Pets.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class VirtualPet
+    {
+        
+        //Fields
+        private int hunger = 5;
+        private int thirst = 5;
+        private int play = 5;
+        private int mood = 5;
+        
+        //Properties
+        public int hungerLevel { get; set; }
+        public int thirstLevel { get; set; }
+        public int playLevel { get; set; }
+        public int moodLevel { get; set; }
+
+        //Constructors
+        public VirtualPet()
+        {
+
+        }
+        public VirtualPet(int hungerLevel, int thirstLevel, int playLevel, int moodLevel)
+        {
+          
+        }
+        //Methods
+        public void Food()
+        {
+            hungerLevel = hungerLevel + 5;
+            thirstLevel = thirstLevel + 5;
+            playLevel = playLevel + 2;
+            moodLevel = moodLevel + 3;
+            if (hungerLevel > 5)
+            {
+                Console.WriteLine("The BoogyMan is feeling good and ready to play");
+            }
+            else if (hungerLevel <=5)
+            {
+                Console.WriteLine("The BoogyMan is still hungry!");
+            }
+            else if (hungerLevel < 0)
+            {
+                Console.WriteLine("You are taking food away from The BoogyMan! The BoogyMan is gonna get you!!");
+            }
+            else
+            {
+                Console.WriteLine("The BoogyMan wants to scare someone.");
+            }
+
+        }
+        public void Water()
+        {
+            hungerLevel = hungerLevel +5;
+            thirstLevel = thirstLevel + 6;
+            playLevel = playLevel - 1;
+            moodLevel = moodLevel + 3;
+            if (thirstLevel > 5)
+            {
+                Console.WriteLine("The BoogyMan is feeling good and needs to play.");
+            }
+            else
+            {
+                Console.WriteLine("The BoogyMan is still thirsty. Hurry before he comes for you!");
+            }
+        }
+        public void Play()
+        {
+            hungerLevel = hungerLevel - 3;
+            thirstLevel = thirstLevel - 3;
+            playLevel = playLevel + 5;
+            moodLevel = moodLevel + 5;
+            Console.WriteLine("The BoogyMan had fun but now he is hungry and thirsty.");
+            if (playLevel > 8)
+            {
+                Console.WriteLine("You better feed him before The BoogyMan gets you");
+                Console.WriteLine();
+
+            }
+            
+        }
+        public void Mood()
+        {
+            hungerLevel = hungerLevel + 5;
+            thirstLevel = thirstLevel + 5;
+            playLevel = playLevel +5;
+            moodLevel = moodLevel + 5;
+        }
+        public void Status()
+        {
+            int changes;
+            Random levels = new Random();
+            changes = levels.Next(1,10);
+
+            if (changes <= 5 && changes > 0)
+            {
+                hunger = hungerLevel + changes;
+                thirst = thirstLevel + changes;
+                play = playLevel + changes;
+                mood = moodLevel + changes;
+
+                Console.WriteLine("The BoogyMan feels different now. This is what he needs now. Before he comes for you!");
+
+                //Console.WriteLine("Hunger Level: {0}", hungerLevel);
+                //Console.WriteLine("Thirsrt Level: {0}", thirstLevel);
+                //Console.WriteLine("Play Level: {0}", playLevel);
+                //Console.WriteLine("Mood Level: {0}", moodLevel);
+            }
+            else
+            {
+                hungerLevel = hunger + 2;
+                thirstLevel = thirst + 2;
+                playLevel = play + 2;
+                moodLevel = mood + 2;
+              
+
+                Console.WriteLine("This is how he feels now.");
+
+            }   Console.WriteLine("Hunger Level: {0}", hungerLevel);
+                Console.WriteLine("Thirsrt Level: {0}", thirstLevel);
+                Console.WriteLine("Play Level: {0}", playLevel);
+                Console.WriteLine("Mood Level: {0}", moodLevel);
+        }
+
+    }
+    
+}
---
+++ VirtualPet/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            VirtualPet boogyMan = new VirtualPet(5, 5, 5, 5);
+
+            Console.WriteLine("This is The BoogyMan my pet monster");
+            Console.WriteLine("The BoogyMan is a monster that needs to be fed, given water, and played with.");
+            Console.WriteLine("All of these things affects The BoogyMan's mood. Keep him happy or he will get you!.");
+            Console.WriteLine();
+            Console.WriteLine("Hit enter to take care of The BoogyMan");
+            Console.ReadKey();
+            Console.WriteLine();
+            boogyMan.Status();
+            Console.WriteLine();
+
+           
+
+            while (true)
+            {
+                Console.WriteLine("What do you want to do for The BoogyMan?");
+                Console.WriteLine("Enter 1 to feed him.");
+                Console.WriteLine("Enter 2 to give him water.");
+                Console.WriteLine("Enter 3 to play with him.");
+                Console.WriteLine("Enter 4 to simply do nothing.");
+
+                int choice = Convert.ToInt32(Console.ReadLine());
+                if (choice == 1)
+                {
+                    boogyMan.Food();
+                }
+                else if (choice == 2)
+                {
+                    boogyMan.Water();
+                }
+                else if (choice == 3)
+                {
+                    boogyMan.Play();
+                }
+                else if (choice == 4)
+                {
+                    Console.WriteLine("BOO...BOOO...The BoogyMan is going to get you!!!");
+                    Console.ReadKey();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a number between 1 and 4");
+                }
+
+                Console.WriteLine("This is how he feels now.");
+                Console.WriteLine();
+                Console.WriteLine("Hunger Level: {0}", boogyMan.hungerLevel);
+                Console.WriteLine("Thirst Level: {0}", boogyMan.thirstLevel);
+                Console.WriteLine("Play Level: {0}", boogyMan.playLevel);
+                Console.WriteLine("Mood Level: {0}", boogyMan.moodLevel);
+                Console.ReadKey();
+
+                Console.WriteLine();
+                Console.WriteLine("Do you want to continue? Enter 1 for Yes and 2 for No");
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                if (option == 1)
+                {
+                    continue;
+                }
+                else if (option == 2)
+                {
+                    Console.WriteLine("Boo...Booo! The BoogyMan is going to get you!!");
+                    Console.ReadKey();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter 1 or 2.");
+                    option = Convert.ToInt32(Console.ReadLine());
+                }
+
+
+
+            }
+
+            
+
+        }
+    }
+}
